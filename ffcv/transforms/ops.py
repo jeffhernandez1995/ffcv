8729f27f818e06d9f049d85c4882add047e3eb3a
jeffhernandez1995 <jeffhernandez1995@gmail.com>
2024-02-25 13:34:36 -0600

    Test

--- ffcv/transforms/ops.py
+++ ffcv/transforms/ops.py
@@ -52,6 +52,7 @@ class ToDevice(Operation):
                     dst = dst.permute(0, 3, 1, 2)
             dst = dst[:inp.shape[0]]
             dst.copy_(inp, non_blocking=self.non_blocking)
+            print(dst.device)
             return dst
 
         return to_device
